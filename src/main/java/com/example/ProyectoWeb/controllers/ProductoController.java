8a2b2f6fac553e36f1f400b2766959046f96f487
Pablo Guartazaca <guqupa@gmail.com>
2025-07-14 16:22:59 -0500

    Inicial

--- src/main/java/com/example/ProyectoWeb/controllers/ProductoController.java
+++ src/main/java/com/example/ProyectoWeb/controllers/ProductoController.java
@@ -3,6 +3,7 @@ package com.example.ProyectoWeb.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,4 +51,13 @@ public class ProductoController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        return productoRepository.findById(id)
+                .map(p -> {
+                    productoRepository.delete(p);
+                    return ResponseEntity.ok().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
